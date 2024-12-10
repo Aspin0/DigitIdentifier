f0dff01183393a5ef2703fff3f3da6397b9d69be
Lucas <lucasjasp@outlook.com>
2024-12-10 13:48:56 +0000

    Add project files.

---
+++ DigitIdentifier/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DigitIdentifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.canvas.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.canvas.Location = new System.Drawing.Point(11, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(426, 426);
+            this.canvas.TabIndex = 0;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Predict";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(485, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Prediction:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(523, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 33);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(495, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Loading...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(633, 475);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DigitIdentifier/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace DigitIdentifier
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmap;
+        Boolean cursorMoving = false;
+        Pen cursorPen;
+        int cursorX = -1;
+        int cursorY = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Visible = false;
+            bmap = new Bitmap(canvas.Width, canvas.Height);
+            // start with bitmap as white otherwise first save will be transparent
+            using (Graphics g = Graphics.FromImage(bmap))
+            {
+                g.Clear(Color.White);
+            }
+
+            cursorPen = new Pen(Color.Black, 40);
+            cursorPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            cursorPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        // Clear button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(bmap)) 
+            { 
+                g.Clear(Color.White); 
+            }
+            canvas.Invalidate();
+            label2.Text = "";
+        }
+
+        // Predict button
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+
+            String result = await RunOnSeperateThread();
+
+            label3.Visible = false;
+            label2.Text = result;
+        }
+
+        private async Task<String> RunOnSeperateThread()
+        {
+            return await Task.Run(() => predict());
+        }
+
+        private string predict()
+        {
+            // this is where the drawn image will be saved
+            bmap.Save("C:/Users/Lucas/Documents/DigitsCNNproject/drawing.png");
+
+            var psi = new ProcessStartInfo();
+            // change this to whereever your python.exe is stored 
+            psi.FileName = @"C:\Users\Lucas\AppData\Local\Programs\Python\Python312\python.exe";
+
+            // change this to whereever the CNNScript.py is saved
+            var script = @"C:\Users\Lucas\Documents\DigitsCNNproject\CNNscript.py";
+            psi.Arguments = script;
+
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            var errors = "";
+            var results = "";
+
+            using (var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+                results = process.StandardOutput.ReadToEnd();
+            }
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(results);
+            return results;
+
+            //label2.Text = results;
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            cursorMoving = true;
+            cursorX = e.X;
+            cursorY = e.Y;
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            cursorMoving = false;
+            cursorX = -1;
+            cursorY = -1;
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (cursorX != -1 && cursorY != -1 && cursorMoving == true)
+            {
+                // this draws to the bitmap that will be saved, but is not visible on screen
+                using (Graphics g = Graphics.FromImage(bmap))
+                {
+                    g.DrawLine(cursorPen, new Point(cursorX, cursorY), e.Location);
+                }
+                // this draws the graphics on screen, seperate but identical to the bitmap
+                canvas.CreateGraphics().DrawLine(cursorPen, new Point(cursorX, cursorY), e.Location);
+                cursorX = e.X;
+                cursorY = e.Y;
+            }
+        }
+    }
+}
